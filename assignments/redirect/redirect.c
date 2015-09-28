29ca26b056c6fd37b5dd1717543acd74716a0a22
Jonathan Reyna <jreylbc@gmail.com>
2015-09-28 04:11:44 -0400

    Completed assignments for chapters 3 and 4

--- assignments/redirect/redirect.c
+++ assignments/redirect/redirect.c
@@ -1,3 +1,7 @@
+/* Program 4.18:
+ * A program that redirects standard output to the
+ * file my.file.
+ */
 #include <fcntl.h>
 #include <stdio.h>
 #include <sys/stat.h>
@@ -5,28 +9,61 @@
 
 #include "../restart/restart.h"
 
+/* Define the flags to use when opening my.file
+ *
+ * O_WRONLY: Open for writing only
+ * O_CREAT: Create file if it doesn't already exist
+ * O_APPEND: Writes are always appended to end of file
+ */
 #define CREATE_FLAGS (O_WRONLY | O_CREAT | O_APPEND)
+
+/* Define the bit mask that specifies the access mode
+ * my.file should be created with.
+ *
+ * S_IRUSR: 0400 - User-read
+ * S_IWUSR: 0200 - User-write
+ * S_IRGRP: 040 - Group-read
+ * S_IROTH: 04 - Other-read
+ */
 #define CREATE_MODE (S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH)
 
 int main() {
+    /* my.file's file descriptor */
     int fd;
 
+    /* Open or create my.file */
     fd = open("my.file", CREATE_FLAGS, CREATE_MODE);
+    /* If there was an error opening my.file */
     if (fd == -1) {
+        /* Print an error */
         perror("Failed to open my.file");
+        /* Return 1 to indicate the error to the shell */
         return 1;
     }
+    /* Make a duplicate of my.file's file descriptor using STDOUT_FILENO
+     * (if STDOUT_FILENO is open, close it first).
+     */
     if (dup2(fd, STDOUT_FILENO) == -1) {
+        /* If dup2 returns -1, print an error message */
         perror("Failed to redirect standard output");
+        /* And return 1 to the shell to indicate an error occurred */
         return 1;
     }
+    /* Close the old file descriptor. */
     if (r_close(fd) == -1) {
+        /* If r_close() returned an error, print an error message */
         perror("Failed to close the file");
+        /* Return 1 to the shell to indicate the error condition */
         return 1;
     }
+    /* Write "OK" to STDOUT_FILENO (my.file) */
     if (write(STDOUT_FILENO, "OK", 2) == -1) {
+        /* If an write() returns an error, print an error message */
         perror("Failed in writing to file");
+        /* Return 1 to the shell to indicate the error condition */
         return 1;
     }
+    /* If we made it here, everything went well. So return 0
+     * to the shell to indicate success. */
     return 0;
 }
--- assignments/runback/runback.c
+++ assignments/runback/runback.c
@@ -1,3 +1,7 @@
+/* Program 3.7:
+ * The runback program creates a child process to
+ * execute a command string in the background.
+ */
 #include <stdio.h>
 #include <unistd.h>
 
@@ -5,28 +9,50 @@
 #include "../makeargv/makeargv.h"
 
 int main(int argc, char *argv[]) {
+    /* Used to store the child's PID */
     pid_t childpid;
+    /* The delimiters to use with makeargv() */
     char delim[] = " \t";
+    /* The double pointer to pass for tokenization to makeargv() */
     char **myargv;
 
+    /* Make sure the argument count is two */
     if (argc != 2) {
+        /* If the arg count is not two, print an error */
         fprintf(stderr, "Usage: %s string\n", argv[0]);
+        /* return nonzero value to shell to indicate the error condition */
         return 1;
     }
+    /* create a clone of this process, and store the returned PID */
     childpid = fork();
+    /* If the returned PID is -1, there was a fork() error */
     if (childpid == -1) {
+        /* Print an error message */
         perror("Failed to fork");
+        /* return nonzero value to shell to indicate the error condition */
         return 1;
     }
+    /* If childpid is 0, this is the child's code section */
     if (childpid == 0) {
+        /* Attempt to become a new session leader */
         if (setsid() == -1) {
+            /* Could not become a new session leader, so print an error message */
             perror("Child failed to become a session leader");
         }
+            /* The child process became the leader of a new session.
+             * Tokenize the arguments passed into the parent.
+             */
         else if (makeargv(argv[1], delim, &myargv) == -1) {
+            /* There was an error tokenizing the arguments.
+             * Print an error message indicating the error condition
+             */
             fprintf(stderr, "Child failed to construct argument array\n");
         }
+            /* The child process is the leader of a new session,
+             * and its arguments were tokenized successfully.
+             * Attempt to execute the first token, and pass the array to the argument list. */
         else {
-            execvp(myargv[0], &myargv[0]);
+            execvp(myargv[0], myargv); // the & and [0] are extraneous (passing reference to first element
             perror("Child failed to exec command");
         }
         return 1; /* child should never return */

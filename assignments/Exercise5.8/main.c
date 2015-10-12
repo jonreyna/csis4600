bc3789a386cd7b900a09c2a252a70eb18c3ef88f
Jonathan Reyna <jreylbc@gmail.com>
2015-10-12 05:54:58 -0400

    Commented main. Added images to assignment tex. Added algorithm to assignment tex. Deleted stored binary.

--- assignments/Exercise5.8/main.c
+++ assignments/Exercise5.8/main.c
@@ -7,52 +7,69 @@
 
 /**
  * Cases
- * 1. It's just a file. Print its path and return.
- * 2. It's a directory. Call yourself with the directory path.
+ * 1. It's a file. Place it on the stack to be called after full depth has been reached.
+ * 2. It's a directory (recursive case). Call self with the directory path.
  */
 void depthfirst(const char *path) {
 
     DIR *dirp = NULL;
 
+
+    /* Check if this is the end of the path.
+     * If it is, just return.
+     * Otherwise, open the directory.
+     */
     if (path == NULL || strncmp(path, "", 1) == 0) {
         return;
     } else if ((dirp = opendir(path)) == NULL) {
         return;
     }
 
-    char *files[128];
-
-    char *nodeArr[128];
-    memset(nodeArr, 0, 128);
-
+    // Holds the regular file names in this directory.
+    char *regular_file_names[128];
+    // Keep a count of regular files in this directory.
     int regular_file_count = 0;
-    char abs_path[PATH_MAX];
+    // The current d_name.
+    char abs_path[256];
+    // Directory entry pointer to keep handle to the current element in the stream.
     struct dirent *dentp = NULL;
     while (dentp = readdir(dirp)) {
 
-        if (strcmp(dentp->d_name, ".") == 0 || strcmp(dentp->d_name, "..") == 0)
+        // If the path is the current or parent directory, skip this iteration.
+        if (strcmp(dentp->d_name, ".") == 0 || strcmp(dentp->d_name, "..") == 0) {
             continue;
+        }
 
+        // Store the full path with the file name.
         sprintf(abs_path, "%s/%s", path, dentp->d_name);
 
-        // Check if its a directory
+        // Check if this entry is a directory.
         if (dentp->d_type == DT_DIR) {
-            // visit node
+            // If it is, visit the node.
             printf("%s\n", abs_path);
+            // This is the recursive case, so call self with the path.
             depthfirst(abs_path);
         } else {
-            files[regular_file_count] = (char*) malloc(strlen(abs_path) * sizeof(char*));
-            strcpy(files[regular_file_count++], abs_path);
+            // This is not a directory. Allocate space on the array/stack, and store
+            // the file name to print later.
+            regular_file_names[regular_file_count] = (char *) malloc(strlen(abs_path) * sizeof(char *));
+            strcpy(regular_file_names[regular_file_count++], abs_path);
         }
     }
-    for (--regular_file_count; regular_file_count >= 0; regular_file_count--) {
-        printf("%s\n", files[regular_file_count]);
-        free(files[regular_file_count]);
+    // Print files from the array/stack in FILO order.
+    for (--regular_file_count; regular_file_count >= 0; --regular_file_count) {
+        printf("%s\n", regular_file_names[regular_file_count]);
+        // Free the mallocs!
+        free(regular_file_names[regular_file_count]);
     }
+    // Close this directory stream.
     closedir(dirp);
 }
 
 int main(int argc, char *argv[]) {
-    //    listFiles(argv[1]);
-    depthfirst(".");
+    if (argc != 2) {
+        printf("Usage: %s %s\n", argv[0], "[example directory path]");
+    } else {
+        depthfirst(argv[1]);
+    }
 }
